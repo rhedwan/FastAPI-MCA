386838d269407a24a5584f526966759959c4f369
Ridwan Adeyemo <adesolaridwan2003@gmail.com>
2026-06-13 12:00:00 +0100

    Initial FastAPI app

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, Response
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    age =  2026 - 1995
+    sum = 0 
+    for i in range(1, 100):
+        sum += i
+    return {"message": "Welcome to the FastAPI application!", 
+            "age": age, 
+            "sum": sum}
+
+
+@app.get("/student")
+def student():
+
+    names = ["Sakeenah", "Aishat", "Naheemot", "Aminat"]
+    return {
+        "student": names
+    }
+
+
+@app.get("/student/{student_id}")
+def student(student_id: int):
+
+    names = ["Sakeenah", "Aishat", "Naheemot", "Aminat", "Rodiat"]
+    try: 
+        student_name = names[student_id]
+        return {
+            "student_name": student_name,
+            "serial_number": student_id
+        }
+    except:
+        return Response("User not found", status_code=404)

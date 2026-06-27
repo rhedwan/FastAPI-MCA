cd4cd285a88f739f939583d49d8770917c446713
Ridwan Adeyemo <adesolaridwan2003@gmail.com>
2026-06-27 18:56:26 +0100

    Implement CRUD operations for Todo API with create, read, and update functionalities

--- main.py
+++ main.py
@@ -21,10 +21,48 @@ app = FastAPI(title="Todo API")
     response_model=TodoResponse,
     status_code= status.HTTP_201_CREATED,
 )
-
 def create_todo(payload:TodoCreate, db: Session = Depends(get_db)):
     todo = Todo(**payload.model_dump())
     db.add(todo)
     db.commit()
     db.refresh(todo)
     return todo
+
+
+#READ (all)
+@app.get("/todos", response_model=list[TodoResponse])
+def list_todos(db: Session = Depends(get_db)):
+    return db.query(Todo).all()
+
+
+
+#  READ (one)
+@app.get('/todos/{todo_id}', response_model=TodoResponse)
+def get_todo(todo_id:int, db: Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found!")
+    
+    return todo
+
+
+# UPDATE
+@app.put('/todos/{todo_id}', response_model=TodoResponse)
+def get_todo(todo_id:int, payload: TodoUpdate, db: Session = Depends(get_db)):
+
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found!")
+    
+
+
+    for field, value in payload.model_dump(exclude_unset=True).items():
+        setattr(todo, field, value)
+
+    
+    db.commit()
+    db.refresh(todo)
+    return todo
+
